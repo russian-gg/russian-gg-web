1b1f3bb27b1eb15703e0cbf4c6548cd099a61ba5
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2026-08-01 23:05:15 +0500

    Use official Google G asset

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -379,23 +379,80 @@ function GoogleContinueButton({
 
 function GoogleGlyph() {
   return (
-    <svg viewBox="0 0 48 48" aria-hidden="true" className="mr-3 size-5 shrink-0">
-      <path
-        fill="#FFC107"
-        d="M43.6 20.1H42V20H24v8h11.3C33.7 32.7 29.2 36 24 36c-6.6 0-12-5.4-12-12S17.4 12 24 12c3.1 0 5.8 1.2 8 3l5.7-5.7C34 6.1 29.3 4 24 4 13 4 4 13 4 24s9 20 20 20 20-9 20-20c0-1.3-.1-2.7-.4-3.9Z"
-      />
-      <path
-        fill="#FF3D00"
-        d="m6.3 14.7 6.6 4.8C14.7 16.1 19 13 24 13c3.1 0 5.8 1.2 8 3l5.7-5.7C34 6.1 29.3 4 24 4c-7.7 0-14.3 4.3-17.7 10.7Z"
-      />
-      <path
-        fill="#4CAF50"
-        d="M24 44c5.2 0 9.9-2 13.4-5.2l-6.2-5.2c-2.1 1.5-4.5 2.4-7.2 2.4-5.2 0-9.6-3.3-11.2-8l-6.5 5C9.7 39.5 16.3 44 24 44Z"
-      />
-      <path
-        fill="#1976D2"
-        d="M4 24c0 3.1.7 6 2.3 8.7l6.5-5c-.4-1.1-.8-2.4-.8-3.7s.3-2.6.8-3.7l-6.5-5A19.8 19.8 0 0 0 4 24Z"
-      />
+    <svg viewBox="12 10 20 20" aria-hidden="true" className="mr-3 size-5 shrink-0">
+      <mask id="google-g-mask" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="12" y="10" width="20" height="20">
+        <path d="M31.3987 18.1814H21.9849V22.0445H27.3598C27.1286 23.294 26.4294 24.3596 25.3676 25.0712C24.4746 25.6716 23.3266 26.0211 21.9849 26.0211C19.3864 26.0211 17.1823 24.2666 16.3947 21.9004C16.1952 21.2989 16.0853 20.6599 16.0853 19.9983C16.0853 19.3367 16.1952 18.6966 16.3947 18.0962C17.1823 15.7311 19.3864 13.9755 21.9849 13.9755C23.4524 13.9755 24.767 14.4816 25.8039 15.4713L28.6653 12.6057C26.936 10.9908 24.6786 10 21.9849 10C18.0832 10 14.705 12.2414 13.0618 15.5076C12.383 16.8592 12 18.3834 12 19.9994C12 21.6155 12.383 23.1396 13.0618 24.4913C14.705 27.7597 18.0832 30 21.9849 30C24.6797 30 26.9485 29.1137 28.6018 27.5861C30.4887 25.8452 31.5732 23.2702 31.5732 20.2275C31.5732 19.5182 31.5131 18.835 31.3987 18.1825V18.1814Z" fill="#E94FFF" />
+      </mask>
+      <g mask="url(#google-g-mask)">
+        <g filter="url(#google-g-blur-main)">
+          <g clipPath="url(#google-g-clip)" data-figma-skip-parse="true">
+            <g transform="matrix(0.00804129 -0.00805186 0.00804128 0.00805186 21.6819 19.7927)">
+              <foreignObject x="-2105.64" y="-2105.64" width="4211.29" height="4211.29">
+                <div xmlns="http://www.w3.org/1999/xhtml" style={{ background: 'conic-gradient(from 90deg,rgba(255, 70, 65, 1) 0deg,rgba(255, 70, 65, 1) 4.14555deg,rgba(49, 134, 255, 1) 39.154deg,rgba(49, 134, 255, 1) 72.0044deg,rgba(0, 165, 183, 1) 96.7463deg,rgba(14, 188, 95, 1) 120.897deg,rgba(14, 188, 95, 1) 154.722deg,rgba(108, 196, 0, 1) 179.136deg,rgba(255, 204, 0, 1) 203.588deg,rgba(255, 211, 20, 1) 226.915deg,rgba(255, 204, 0, 1) 251.688deg,rgba(255, 106, 43, 1) 273.129deg,rgba(253, 70, 65, 1) 289.305deg,rgba(255, 70, 65, 1) 359.593deg,rgba(255, 70, 65, 1) 360deg)', height: '100%', width: '100%', opacity: 1 }} />
+              </foreignObject>
+            </g>
+          </g>
+          <path d="M9.25923 19.7927C9.25923 12.6759 15.0209 6.90668 22.1283 6.90668C29.2357 6.90668 34.9973 12.6759 34.9973 19.7927C34.9973 26.9094 29.2357 32.6786 22.1283 32.6786C15.0209 32.6786 9.25923 26.9094 9.25923 19.7927Z" />
+        </g>
+        <g filter="url(#google-g-blur-blue-small)">
+          <ellipse cx="22.0496" cy="20.2413" rx="5.39634" ry="2.83537" transform="rotate(24.4473 22.0496 20.2413)" fill="#3186FF" />
+        </g>
+        <g filter="url(#google-g-blur-blue-wide)">
+          <ellipse cx="35.3538" cy="18.2155" rx="7.43918" ry="3.09357" fill="#3186FF" />
+        </g>
+        <g filter="url(#google-g-blur-red)">
+          <ellipse cx="27.2744" cy="16.2195" rx="7.40854" ry="2.37805" fill="#FF4641" />
+        </g>
+        <g filter="url(#google-g-blur-pink)">
+          <ellipse cx="31.5427" cy="12.9268" rx="7.40854" ry="2.37805" fill="#FF5B8B" />
+        </g>
+        <g filter="url(#google-g-blur-blue-mid)">
+          <ellipse cx="26.4817" cy="19.878" rx="8.5061" ry="3.10976" fill="#3186FF" />
+        </g>
+        <g filter="url(#google-g-blur-red-small)">
+          <ellipse cx="27.1842" cy="14.0197" rx="4.53882" ry="2.37805" transform="rotate(-28.6599 27.1842 14.0197)" fill="#FF4641" />
+        </g>
+      </g>
+      <defs>
+        <filter id="google-g-blur-main" x="7.25922" y="4.90668" width="29.7381" height="29.772" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <clipPath id="google-g-clip">
+          <path d="M9.25923 19.7927C9.25923 12.6759 15.0209 6.90668 22.1283 6.90668C29.2357 6.90668 34.9973 12.6759 34.9973 19.7927C34.9973 26.9094 29.2357 32.6786 22.1283 32.6786C15.0209 32.6786 9.25923 26.9094 9.25923 19.7927Z" />
+        </clipPath>
+        <filter id="google-g-blur-blue-small" x="14.9977" y="14.828" width="14.1037" height="10.8265" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="google-g-blur-blue-wide" x="25.9146" y="13.1219" width="18.8784" height="10.1871" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="google-g-blur-red" x="17.8658" y="11.8415" width="18.8171" height="8.7561" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="google-g-blur-pink" x="22.1342" y="8.54878" width="18.8171" height="8.7561" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="google-g-blur-blue-mid" x="15.9756" y="14.7683" width="21.0122" height="10.2195" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+        <filter id="google-g-blur-red-small" x="21.0403" y="9.0042" width="12.2878" height="10.0309" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur" />
+        </filter>
+      </defs>
     </svg>
   )
 }
