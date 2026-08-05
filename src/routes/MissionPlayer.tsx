9c1211569380165f7d0709cbb830429ee5b75008
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2026-08-05 21:42:52 +0500

    Lower mission mic block a little further

--- src/routes/MissionPlayer.tsx
+++ src/routes/MissionPlayer.tsx
@@ -1291,7 +1291,7 @@ function MicControl({
      * the spot the learner last reached for. The reserved space costs nothing: it is empty
      * screen either way.
      */
-    <div className="mt-7 min-h-48 md:mt-6">
+    <div className="mt-9 min-h-48 md:mt-8">
       <div className="relative flex items-center justify-center">
         {/*
           Rings leaving the microphone while it is being spoken into. They sit behind the
